383b73eae3760d8d4aeee79c7f4db10ba0319ac8
jhson0531 <42314873+jhson0531@users.noreply.github.com>
2024-05-24 01:08:41 +0900

    Update p9inheritMain.cpp

--- hw_12/practice5/p9inheritMain.cpp
+++ hw_12/practice5/p9inheritMain.cpp
@@ -6,8 +6,12 @@ int main() {
 	animal* a[3];
 	for (int i = 0;i < 2;i++) {
 		a[i] = new fish();
+		cout << "a+i : " << a + i << endl;
+		cout << "a[i] : " << a[i] << endl;
 	}
 	fish c;
+
+	cout << "fish c : " << &c << endl;
 	
 	a[2] = &c;
 
